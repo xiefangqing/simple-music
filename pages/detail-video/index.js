941dbf75e0284ca19ceb70bf04bf5896623b4ee2
xfq <389368595@qq.com>
2022-04-10 20:23:45 +0800

    添加 视频详情页基本结构

--- pages/detail-video/index.js
+++ pages/detail-video/index.js
@@ -1,7 +1,23 @@
+import {
+  getMVUrl,
+  getMVDetail,
+  getRelatedAllVideo
+} from '../../service/api/video'
+
 Page({
-    data: {},
-    onLoad: function (options) {
-      console.log(options)
-      
-    }
+  data: {
+    mvUrlData: {},
+    mvDetailData: {},
+    mvRelatedAllVideoData: []
+  },
+  onLoad(options) {
+    const id = options.id
+    this.getPageData(id)
+  },
+  getPageData(id) {
+    // 并行提高效率（不应该用 Promise.all ）
+    getMVUrl(id).then(res => this.setData({ mvUrlData: res.data }))
+    getMVDetail(id).then(res => this.setData({ mvDetailData: res.data }))
+    getRelatedAllVideo(id).then(res => this.setData({ mvRelatedAllVideoData: res.data }))
+  }
 });
--- service/api/video.js
+++ service/api/video.js
@@ -1,8 +1,26 @@
 import request from '../../service/index.js'
 
-export function getTopMV (offset, limit = 10) {
+export function getTopMV(offset, limit = 10) {
   return request.get('/top/mv', {
     offset,
     limit
+  })
+}
+
+export function getMVUrl(id) {
+  return request.get('/mv/url', {
+    id
+  })
+}
+
+export function getMVDetail(id) {
+  return request.get('/mv/detail', {
+    mvid: id
+  })
+}
+
+export function getRelatedAllVideo(id) {
+  return request.get('/related/allvideo', {
+    id
   })
 }
